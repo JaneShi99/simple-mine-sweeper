936e992faae2967a7393869380636372b76b6cce
Shi Shi <j96shi@uwaterloo.ca>
2018-12-22 11:49:37 -0700

    fixed some lines for cell

--- cell.h
+++ cell.h
@@ -9,17 +9,23 @@ class Cell{
     bool initialized;
 
     public:
-    void setIsBomb(bool b){isBomb = b;}
+
+    Cell():isBomb{false},isRevealed{false},bombsAroundMe{0},initialized{false}{}
     
+    void addNeighbour(Cell * n ){neighbours.emplace_back(n);}
+
+    void setIsBomb(bool b){isBomb = b;}
+
     void setIsRevealed(bool b){isRevealed = b;}
 
     void countBombsAroundMe(){
-      if(!initialized)
-      {
-        for(auto& it: neighbours){
-            bombsAroundMe += it->notify();
+        if(!initialized)
+        {
+            for(auto& it: neighbours){
+                bombsAroundMe += it->notify();
+            }
         }
-    }
+        initialized = true;
     }
     int notify(){return isBomb? 1 :  0;}
 };
---
+++ game.h
@@ -0,0 +1,13 @@
+#include "cell.h"
+#include<iostream>
+
+class game{
+    std::vector<std::vector<Cell>> board;
+    bool gameOn;
+    bool win;
+
+   private:
+    
+
+
+}
